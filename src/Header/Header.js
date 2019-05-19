2d119686b6d0aca38e39ddddfdbc0a33f2e97ed3
Gabriel Jucht <gabrieljucht@gmail.com>
2019-05-19 18:34:47 -0300

    primer commit

---
+++ src/Header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const header = (props) => {
+  return <header><nav><ul><li>Nosotros</li><li>Juegos</li><li>Contacto</li></ul></nav></header>
+}
+
+export default header;
