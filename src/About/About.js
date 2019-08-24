38e4fd0e6677a610bfdbbe8dec82a47ef92f1e79
Gabriel Jucht <gabrieljucht@gmail.com>
2019-08-24 11:24:36 -0300

    cambios de contenido about, cambio de link del header

--- src/About/About.js
+++ src/About/About.js
@@ -10,9 +10,7 @@ const about = (props) => {
               <img src={image} alt=""/>
             </div>
             <div className="texto col-sm-8">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec enim risus, viverra eu facilisis sit amet, volutpat ac nisl. Suspendisse potenti. In tincidunt lobortis sem non pellentesque. Vivamus convallis id est sed vehicula. Aliquam quam est, vestibulum varius laoreet feugiat, elementum quis leo. Integer diam urna, sodales nec mattis sed, porta a leo. Sed eros urna, posuere sit amet ligula non, sollicitudin volutpat est. Mauris at dictum leo. Aenean finibus lectus at leo bibendum ultricies. In hac habitasse platea dictumst.</p>
-                <p>Nulla eu commodo tellus, sit amet vestibulum enim. Maecenas vitae ornare nulla, convallis cursus tellus. Etiam sodales accumsan vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis faucibus sapien tincidunt justo tincidunt, nec rhoncus ligula facilisis. In hac habitasse platea dictumst. Praesent arcu mi, sagittis eu augue porttitor, lobortis condimentum dolor.</p>
-                <p>Nulla eu commodo tellus, sit amet vestibulum enim. Maecenas vitae ornare nulla, convallis cursus tellus. Etiam sodales accumsan vehicula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis faucibus sapien tincidunt justo tincidunt, nec rhoncus ligula facilisis. In hac habitasse platea dictumst. Praesent arcu mi, sagittis eu augue porttitor, lobortis condimentum dolor.</p>
+                <p>When you work as a programmer and love videogames is only a small step that make you try to make one of your own. We love story driven games so this turns to be our goal.</p>
             </div>
          </div>
          </div>
